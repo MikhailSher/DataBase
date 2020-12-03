00783c561518e2c61ad10761dd8901a429eaa0d4
Misha <mikhailsochi17@gmail.com>
2020-12-03 21:32:24 +0300

    DBconnection

---
+++ src/main/java/ru/db/servlets/ServletForUsers.java
@@ -0,0 +1,65 @@
+package ru.db.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+/**
+ * 15.10.2020
+ * ServletForUsers
+ *
+ * @author Sherstobitov Mikhail
+ * @version v1.0
+ */
+@WebServlet("/users")
+public class ServletForUsers extends HttpServlet {
+
+    private Connection connection;
+    @Override
+    public void init() throws ServletException {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/db.properties")));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUsername = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String driverClassName = properties.getProperty("db.driverClassName");
+
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+        } catch (IOException | SQLException | ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getServletContext().getRequestDispatcher("/jsp/addUser.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("first-name");
+        String lastName = req.getParameter("last-name");
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO " +
+                            "user(first_name, last_name) VALUES (?, ?)");
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+        req.getServletContext().getRequestDispatcher("/jsp/addUser.jsp").forward(req, resp);
+
+    }
+}
